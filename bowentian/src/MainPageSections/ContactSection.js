5673b863e8b28234769668bf4773822489bc87d3
bowen.tian <bowen.tian@mail.mcgill.ca>
2021-11-29 21:42:14 -0500

    email modification

--- bowentian/src/MainPageSections/ContactSection.js
+++ bowentian/src/MainPageSections/ContactSection.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './css/MainPageSections.css'
-import './css/CareerSection.css'
+import './css/ContactSection.css'
+import emailjs from "emailjs-com"
 
 export default class ContactSection extends Component {
     constructor() {
@@ -8,11 +9,29 @@ export default class ContactSection extends Component {
     }
 
     render() {
+        function sendEmail(e) {
+            e.preventDefault();
+
+            // emailjs.sendForm('service_44nuhq2', 'template_5em7z8a', e.target, 'user_aMG6OWs4B2oNCbq2nGVNl')
+            //     .then((result) => {
+            //         console.log(result.text);
+            //     }, (error) => {
+            //         console.log(error.text);
+            //     });
+            // e.target.reset()
+        }
+
         return (
-            <div className="SectionContainer DefaultBackground">
-                <div>Send me a message.</div>
+            <div className="SectionContainer DefaultBackground ContactSectionContainer">
+                <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                          <textarea className="form-control ContactSectionTextArea" id=""
+                                    placeholder="Send me a message." name="message"/>
+                        <input type="submit" className="btn btn-info ContactSectionSendButton" value="Send"/>
+                    </div>
+                </form>
             </div>
         );
     }
-  
+
 }
